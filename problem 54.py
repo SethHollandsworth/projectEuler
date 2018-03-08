c5fbf8135942ffbda6458a43a925a89eeedb094f
srhollan <srhollan@ncsu.edu>
2018-03-08 16:13:22 -0500

    updated problem 54

--- problem 54.py
+++ problem 54.py
@@ -1,5 +1,5 @@
 import itertools
-#TODO having scores equals for one pair, etc
+# TODO having scores equals for one pair, etc
 # Clubs = 0, Diamonds = 1, Hearts = 2, Spades = 3
 # nums are normal, jack = 11, queen = 12, king = 13, ace = 14
 
@@ -9,55 +9,90 @@ import itertools
 # 7C 8C 5C QD 6C
 
 cardVals = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-suitVals = {'C':0,'D':1,'H':2,'S':3}
-faceCards = {'T':10, 'J':11,'Q':12,'K':13,'A':14}
+suitVals = {'C': 0, 'D': 1, 'H': 2, 'S': 3}
+faceCards = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+
 
 def detHands():
     with open('p054_poker.txt') as f:
         content = f.readlines()
         # you may also want to remove whitespace characters like `\n` at the end of each line
         content = [x.strip() for x in content]
-        player1Score = 0
-        for i in content:
-            hand1 = i[:15].strip()
-            hand2 = i[14:].strip()
-            # print(hand1)
-            # print(hand2)
-            hand1 = getScore(convToNumbers(hand1))
-            hand2 = getScore(convToNumbers(hand2))
-            
-            if hand1[0] > hand2[0]:
-                player1Score += 1
-            elif hand1[0] == hand2[0] and hand1[1] > hand2[1]:
-                player1Score += 1
+        # print("test")
+    player1Score = 0
+    player2Score = 0
+    for i in content:
+        hand1 = i[:15].strip()
+        hand2 = i[14:].strip()
+        # print(hand1)
+        # print(hand2)
+        hand1Vals = getScore(convToNumbers(hand1))
+        hand2Vals = getScore(convToNumbers(hand2))
+        # print("hand1Vals")
+        if hand1Vals[0] > hand2Vals[0]:
+            player1Score += 1
+        elif hand1Vals[0] == hand2Vals[0] and hand1Vals[1] > hand2Vals[1]:
+            player1Score += 1
+        elif hand1Vals[0] == hand2Vals[0] == 0 and highestCard(convToNumbers(hand1)) > highestCard(convToNumbers(hand2)):
+            player1Score += 1
+        elif hand1Vals[0] == hand2Vals[0] and hand1Vals[1] == hand2Vals[1] and highestCard(convToNumbers(hand1)) > highestCard(convToNumbers(hand2)):
+            player1Score += 1
 
-            # print(hand1)
-            # print(hand2)
+        if hand1Vals[0] < hand2Vals[0]:
+            player2Score += 1
+        elif hand1Vals[0] == hand2Vals[0] and hand1Vals[1] < hand2Vals[1]:
+            player2Score += 1
+        elif hand1Vals[0] == hand2Vals[0] == 0 and highestCard(convToNumbers(hand1)) < highestCard(convToNumbers(hand2)):
+            player2Score += 1
+        elif hand1Vals[0] == hand2Vals[0] and hand1Vals[1] == hand2Vals[1] and highestCard(convToNumbers(hand1)) < highestCard(convToNumbers(hand2)):
+            player2Score += 1
+        #elif hand1Vals[0] == hand2Vals[0] and not hand1Vals[1] < hand2Vals[1]:
+            #print(hand1)
+            #print(hand2)
 
+        # print(hand1)
+        # print(hand2)
 
-        #print(content)
-    return player1Score
+        # print(content)
+    return (player1Score,player2Score)
+
 
 def getScore(vals):
     score = 0
-    if twoOfAKind(vals):
+    priority = 0
+    one = twoOfAKind(vals)
+    two = twoPairs(vals)
+    three = threeOfAKind(vals)
+    four = straight(vals)
+    five = fullHouse(vals)
+    six = fourOfAKind(vals)
+    seven = straightFlush(vals)
+    eight = royalFlush(vals)
+    if one[0]:
         score = 1
-    if twoPairs(vals):
+        priority = one[1]
+    if two[0]:
         score = 2
-    if threeOfAKind(vals):
+        priority = two[1]
+    if three[0]:
         score = 3
-    if straight(vals):
+        priority = three[1]
+    if four[0]:
         score = 4
-    if fullHouse(vals):
+        priority = four[1]
+    if five:
         score = 5
-    if fourOfAKind(vals):
+        priority = highestCard(vals)
+    if six[0]:
         score = 6
-    if straightFlush(vals):
+        priority = six[1]
+    if seven[0]:
         score = 7
-    if royalFlush(vals):
+        priority = seven[1]
+    if eight:
         score = 8
-    highestCard = sorted(vals[::2])[-1]
-    return (score,highestCard)
+    return (score, priority)
+
 
 def convToNumbers(hand):
     handNums = []
@@ -69,10 +104,12 @@ def convToNumbers(hand):
         elif i in faceCards:
             handNums.append(faceCards[i])
     return handNums
-    
 
 
-        
+def highestCard(vals):
+    nums = vals[::2]
+    return max(nums)
+
 
 def twoOfAKind(vals):
     nums = vals[::2]
@@ -80,20 +117,21 @@ def twoOfAKind(vals):
     # print(list(combos))
     for i in combos:
         if i[0] == i[1]:
-            return True
-    return False
+            return (True, i[0])
+    return (False, 0)
 
 
 def twoPairs(vals):
     nums = vals[::2]
-    count = 0
+    count = []
     combos = itertools.combinations(nums, 2)
     for i in combos:
         if i[0] == i[1]:
-            count += 1
-            if count == 2:
-                return True
-    return False
+            count.append(i[0])
+            if len(count) == 2:
+
+                return (True, sorted(count)[-1])
+    return (False, 0)
 
 
 def threeOfAKind(vals):
@@ -101,17 +139,17 @@ def threeOfAKind(vals):
     combos = itertools.combinations(nums, 3)
     for i in combos:
         if i[0] == i[1] == i[2]:
-            return True
-    return False
+            return (True, i[0])
+    return (False, 0)
 
 
 def straight(vals):
     nums = sorted(vals[::2])
     if nums[1] - nums[0] == nums[2] - nums[1] == nums[3] - nums[2] == nums[4] - nums[3] == 1:
-        return True
+        return (True, max(nums))
     elif nums[1] - nums[0] == nums[2] - nums[1] == nums[3] - nums[2] == 1 and nums[4] == 14:
-        return True
-    return False
+        return (True, nums[-2])
+    return (False, 0)
 
 
 def flush(vals):
@@ -136,42 +174,53 @@ def fourOfAKind(vals):
     combos = itertools.combinations(nums, 4)
     for i in combos:
         if i[0] == i[1] == i[2] == i[3]:
-            return True
-    return False
+            return (True, i[0])
+    return (False, 0)
 
 
 def straightFlush(vals):
-    return straight(vals) and flush(vals)
+    out = straight(vals)
+    return (out[0] and flush(vals),out[1])
+
 
 def royalFlush(vals):
     if flush(vals):
         vals = sorted(vals[::2])
-        #print(cardVals[8:])
-        #print(vals)
+        # print(cardVals[8:])
+        # print(vals)
         return vals == cardVals[8:]
 
-# if __name__ == "__main__":
-#     print("PASS" if twoOfAKind([5, 2, 5, 1, 6, 3, 7, 3, 13, 1]) else "FAIL")
-#     print("PASS" if not twoOfAKind(
-#         [9, 2, 5, 1, 6, 3, 7, 3, 13, 1]) else "FAIL")
-#     print("PASS" if threeOfAKind([5, 2, 5, 1, 6, 3, 5, 3, 13, 1]) else "FAIL")
-#     print("PASS" if not threeOfAKind(
-#         [2, 2, 5, 1, 6, 3, 5, 3, 13, 1]) else "FAIL")
-#     print("PASS" if twoPairs([5, 2, 5, 1, 6, 3, 7, 3, 6, 1]) else "FAIL")
-#     print("PASS" if not twoPairs([9, 2, 5, 1, 6, 3, 7, 3, 13, 1]) else "FAIL")
-#     print("PASS" if straight([5, 2, 6, 1, 7, 3, 8, 3, 9, 1]) else "FAIL")
-#     print("PASS" if straight([10, 2, 11, 1, 12, 3, 13, 3, 14, 1]) else "FAIL")
-#     print("PASS" if straight([14, 2, 2, 1, 3, 3, 5, 3, 4, 1]) else "FAIL")
-#     print("PASS" if not straight([9, 2, 5, 1, 6, 3, 7, 3, 13, 1]) else "FAIL")
-#     print("PASS" if flush([14, 2, 2, 2, 3, 2, 5, 2, 4, 2]) else "FAIL")
-#     print("PASS" if not flush([9, 2, 5, 1, 6, 3, 7, 3, 13, 1]) else "FAIL")
-#     print("PASS" if fullHouse([5, 2, 5, 1, 6, 3, 5, 3, 6, 1]) else "FAIL")
-#     print("PASS" if not fullHouse([5, 2, 5, 1, 6, 3, 5, 3, 13, 1]) else "FAIL")
-#     print("PASS" if fourOfAKind([5, 2, 5, 1, 6, 3, 5, 3, 5, 1]) else "FAIL")
-#     print("PASS" if not fourOfAKind(
-#         [2, 2, 5, 1, 6, 3, 5, 3, 13, 1]) else "FAIL")
-#     print("PASS" if straightFlush([14, 2, 2, 2, 3, 2, 5, 2, 4, 2]) else "FAIL")
-#     print("PASS" if not straightFlush([9, 2, 5, 1, 6, 3, 7, 3, 13, 1]) else "FAIL")
-#     print("PASS" if royalFlush([10, 2, 11, 2, 12, 2, 13, 2, 14, 2]) else "FAIL")
+
+if __name__ == "__main__":
+    print("PASS" if twoOfAKind([5, 2, 5, 1, 6, 3, 7, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if not twoOfAKind(
+        [9, 2, 5, 1, 6, 3, 7, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if threeOfAKind(
+        [5, 2, 5, 1, 6, 3, 5, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if not threeOfAKind(
+        [2, 2, 5, 1, 6, 3, 5, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if twoPairs([5, 2, 5, 1, 6, 3, 7, 3, 6, 1])[0] else "FAIL")
+    print("PASS" if not twoPairs(
+        [9, 2, 5, 1, 6, 3, 7, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if straight([5, 2, 6, 1, 7, 3, 8, 3, 9, 1])[0] else "FAIL")
+    print("PASS" if straight(
+        [10, 2, 11, 1, 12, 3, 13, 3, 14, 1])[0] else "FAIL")
+    print("PASS" if straight([14, 2, 2, 1, 3, 3, 5, 3, 4, 1])[0] else "FAIL")
+    print("PASS" if not straight(
+        [9, 2, 5, 1, 6, 3, 7, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if flush([14, 2, 2, 2, 3, 2, 5, 2, 4, 2]) else "FAIL")
+    print("PASS" if not flush([9, 2, 5, 1, 6, 3, 7, 3, 13, 1]) else "FAIL")
+    print("PASS" if fullHouse([5, 2, 5, 1, 6, 3, 5, 3, 6, 1]) else "FAIL")
+    print("PASS" if not fullHouse(
+        [5, 2, 5, 1, 6, 3, 5, 3, 13, 1]) else "FAIL")
+    print("PASS" if fourOfAKind([5, 2, 5, 1, 6, 3, 5, 3, 5, 1])[0] else "FAIL")
+    print("PASS" if not fourOfAKind(
+        [2, 2, 5, 1, 6, 3, 5, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if straightFlush(
+        [14, 2, 2, 2, 3, 2, 5, 2, 4, 2])[0] else "FAIL")
+    print("PASS" if not straightFlush(
+        [9, 2, 5, 1, 6, 3, 7, 3, 13, 1])[0] else "FAIL")
+    print("PASS" if royalFlush(
+        [10, 2, 11, 2, 12, 2, 13, 2, 14, 2]) else "FAIL")
 
 print(detHands())
